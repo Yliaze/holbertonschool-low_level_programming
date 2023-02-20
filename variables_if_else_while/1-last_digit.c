69afa6b3c40aa04c8ec14db16f18d0c6145f0e8a
Yliaze <benjamin.alazet@gmail.com>
2023-02-20 06:09:03 -0800

    Modify 1-last_digit.c for Betty approval

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@
  * the variable n each time it is executed.
  * Complete the source code in order to print the last digit
  * of the number stored in the variable n.
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
