a95fad690748a2b01c57198cdbfa50d6899ef5f3
Yliaze <benjamin.alazet@gmail.com>
2023-02-20 06:52:58 -0800

    Modify 2-print_alphabet.c for Betty approval

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,7 +4,7 @@
  * main - Alphabet Game
  * Description: Write a program that prints the alphabet
  * in lowercase, followed by a new line.
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
