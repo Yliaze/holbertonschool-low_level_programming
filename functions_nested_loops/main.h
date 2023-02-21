808ca6ea6a36dddd9242796c09a32bf9c11b2c7c
Yliaze <benjamin.alazet@gmail.com>
2023-02-21 07:48:23 -0800

    Add main.h and write a program that prints _putchar, followed by a new line.

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * The prototypes of all my functions and
+ * the prototype of my function _putchar
+ * are contained in this main.h
+ **/
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
