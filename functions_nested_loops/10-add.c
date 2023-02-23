3ca62a0cb9c8cc3b57ec88cc0b8633bdef97b066
Yliaze <benjamin.alazet@gmail.com>
2023-02-23 08:25:48 -0800

    Modify 10-add for betty approval

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
- * add - a function that adds two integers
- * and returns the result.
- *
- * @a: The input number as an integer - a.
- * @b: The input number as an integer - b.
- *
- * Return : the resultat of a + b
- */
+* add - a function that adds two integers
+* and returns the result.
+*
+* @a: number one.
+* @b: number two.
+*
+* Return: Add of number one and number two.
+*/
 int add(int a, int b)
 {
 	return (a + b);
