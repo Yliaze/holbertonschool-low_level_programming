a39ae053dd717d8dbf89bd89144535abc1deffd9
Yliaze <benjamin.alazet@gmail.com>
2023-02-24 00:23:11 -0800

    Second test for 11-print_to_98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,18 +12,21 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-	
 		for (; n > 98; n--)
 		{
-			printf("%d, ", n);
+			_putchar(n);
+			_putchar(' ');
+			_putchar(',');
 		}
 	}
 	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
-			printf("%d, ", n);
+			_putchar(n);
+			_putchar(' ');
+			_putchar(',');
 		}
 	}
-	printf("%d\n", n);
+	_putchar('\n');
 }
