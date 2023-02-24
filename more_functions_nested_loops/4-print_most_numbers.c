d261e834417484db321b6616a648da6061fec5a7
Yliaze <benjamin.alazet@gmail.com>
2023-02-24 04:07:49 -0800

    Modify the 4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
-* print_most_numbers - function that prints the numbers,
-* from 0 to 9
-* Do not print 2 and 4
-*
-* Return: (void)
-*/
+ * print_most_numbers - function that prints the numbers,
+ * from 0 to 9
+ * Do not print 2 and 4
+ *
+ * Return: (void)
+ */
 void print_most_numbers(void)
 {
 	char c = 48;
 
 	while (c <= 57)
-		if (c != 50 && c != 52)
 	{
+		if (c != 50 && c != 52)
+		{
 			_putchar(c);
-			c++;
+		}
+		c++;
 	}
 	_putchar('\n');
 }
