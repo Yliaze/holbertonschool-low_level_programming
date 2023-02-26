d13ea022043e1f72647e12769508ffff80c944e4
Yliaze <benjamin.alazet@gmail.com>
2023-02-26 14:16:36 -0800

    Modify table 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,15 +14,15 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		_putchar('0');
+		_putchar(48);
 		for (b = 1; b <= 9; b++)
 		{
 			c = a * b;
-			_putchar(',');
-			_putchar(' ');
+			_putchar(44);
+			_putchar(32);
 			if (c <= 9)
 			{
-				_putchar(',');
+				_putchar(32);
 				_putchar(c + 48);
 			}
 			else
