b92318adac17d9e0af4c8a6b8aa8849214a578d7
Yliaze <benjamin.alazet@gmail.com>
2023-03-13 08:20:07 -0700

    Modify 2-str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -31,13 +31,13 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i < len1; i++)
 	{
 		p[i] = s1[i];
 	}
-	for (j = 0; s2[j]; j++)
+	for (j = 0; j <= len2; i++, j++)
 	{
-		p[i + j] = s2[j];
+		p[i] = s2[j];
 	}
 	return (p);
 }
