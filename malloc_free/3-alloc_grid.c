ab1404f1aecbad4509bdc7e2d0311ebc1d288f08
Yliaze <benjamin.alazet@gmail.com>
2023-03-14 04:05:12 -0700

    Modify to optimise the code un alloc_grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -28,14 +28,8 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int *) * width);
 
 		if (grid[i] == NULL)
-		{
-			for (j = 0; j < i; j++)
-			{
-				free(grid[j]);
-			}
-			free(grid);
 			return (NULL);
-		}
+
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
