ab8734218c42bee2010e9c6be557833cda5215b1
Yliaze <benjamin.alazet@gmail.com>
2023-03-20 03:50:01 -0700

    Update 2-print

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,9 +14,9 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
