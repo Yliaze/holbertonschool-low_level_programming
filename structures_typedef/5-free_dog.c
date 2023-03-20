5d8ce7d8865f1e70e6b371cbcffaa2c8525ee2e9
Yliaze <benjamin.alazet@gmail.com>
2023-03-20 08:10:56 -0700

    update dog.h and Write a function that frees dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - function that frees dogs
+ *
+ * @d: value to free
+ *
+ * Return: d or NULL if fails
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
