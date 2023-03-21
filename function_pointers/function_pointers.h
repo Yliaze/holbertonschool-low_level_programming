16af31feaaaf516eb8da4a0799a1d7483b8e6c7d
Yliaze <benjamin.alazet@gmail.com>
2023-03-21 02:33:16 -0700

    modify function.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,14 +1,8 @@
-#include <stdio.h>
-#include <unistd.h>
-
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-/**
- * The prototypes of all my functions and
- * the prototype of my functions_pointers
- * are contained in this function_pointers.h
- **/
 
+#include <stdio.h>
+#include <unistd.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
