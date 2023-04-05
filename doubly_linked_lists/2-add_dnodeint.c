df35f5eb9ce5bc0cd1f274612e09537ab1070e9d
Yliaze <benjamin.alazet@gmail.com>
2023-04-05 10:15:36 -0700

    Modify add function for some conditions like head == NULL.

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -18,9 +18,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
 	new->next = *head;
-
+	(*head)->prev = new;
 	*head = new;
-
+	}
 	return (new);
 }
