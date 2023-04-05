f1d934c0ffc5948f08a3eafcab0358b74afcd6ef
Yliaze <benjamin.alazet@gmail.com>
2023-04-05 09:38:08 -0700

    Add list.h, a function that prints all the elements of a dlistint_t list and a function  that returns the number of elements in a linked dlistint_t list.

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all of the elements of a
+ * doubly linked list.
+ *
+ * @h: Pointer to a linked list head.
+ *
+ * Return: The number of total nodes in the
+ * doubly linked list.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *head = NULL;
+	int count = 0;
+
+	head = (dlistint_t *) h;
+
+	while (head != NULL)
+	{
+		printf("%i\n", head->n);
+		head = head->next;
+		count++;
+	}
+	return (count);
+}
---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Returns the number of elements in a
+ * doubly linked list
+ *
+ * @h: Pointer to a linked list head.
+ *
+ * Return: The number of elements in the
+ * doubly linked list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t *head = NULL;
+	int count = 0;
+
+	head = (dlistint_t *) h;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		count++;
+	}
+	return (count);
+}
