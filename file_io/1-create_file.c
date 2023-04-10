255088aa62cb4dc8ecec6ec6fa2422522c009c3b
Yliaze <benjamin.alazet@gmail.com>
2023-04-10 08:09:17 -0700

    Create a function that creates a file.

---
+++ file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ *
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string
+ * to write to the file
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w_fd, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	w_fd = write(fd, text_content, len);
+	if (w_fd == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
