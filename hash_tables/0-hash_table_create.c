c2fc13deb943089320983fbc3c14b4e73cad29ff
Yliaze <benjamin.alazet@gmail.com>
2023-04-11 07:30:37 -0700

    Write a function that creates a hash table and copy/paste a hash function implementing the djb2 algorithm.

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: size of the hash table
+ *
+ * Return: pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table;
+	unsigned long int i;
+
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (new_hash_table == NULL)
+		return (NULL);
+
+	new_hash_table->size = size;
+	new_hash_table->array = malloc(sizeof(hash_table_t) * size);
+	if (new_hash_table->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		new_hash_table->array[i] = NULL;
+	}
+
+	return (hash_table);
+}
