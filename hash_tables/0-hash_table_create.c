e7ee6615d8faf384823a97575a761f6fd2411518
Yliaze <benjamin.alazet@gmail.com>
2023-04-11 07:36:34 -0700

    Fixing allocated memory hash_table_create(2048); -> 16400 bytes

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,14 +17,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_hash_table->size = size;
-	new_hash_table->array = malloc(sizeof(hash_table_t) * size);
+	new_hash_table->array = malloc(sizeof(hash_table_t *) * size);
 	if (new_hash_table->array == NULL)
+	{
+		free(new_hash_table);
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
 		new_hash_table->array[i] = NULL;
 	}
-
 	return (new_hash_table);
 }
