8ca09962d506097ee2862de213f5deaadecc8f29
Yliaze <benjamin.alazet@gmail.com>
2023-04-13 08:31:56 -0700

    Write a function that prints a hash table, commas are hell on earth

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Function that print a hash table
+ *
+ * @ht: the hash table to print
+ *
+ * Return: 0.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node = NULL;
+	long unsigned int i = 0;
+	int first = 1;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	while (i < ht->size)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+		i++;
+	}
+	printf("}\n");
+	return;
+}
